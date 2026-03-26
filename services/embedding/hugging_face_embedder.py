eb09e7784d5dd498857e0339eeb1937720c2f67b
Arina <azerkalova@internet.ru>
2026-03-26 13:18:47 +0700

    Update hugging_face_embedder.py

--- services/embedding/hugging_face_embedder.py
+++ services/embedding/hugging_face_embedder.py
@@ -1,9 +1,10 @@
-from sentence_transformers import SentenceTransformer
+#from sentence_transformers import SentenceTransformer
 from .embedder import Embedder
 
 class HuggingFaceEmbedder(Embedder):
-    def __init__(self, model_name: str = "sentence-transformers/intfloat/e5-base-v2"):
-        self.model = SentenceTransformer(model_name)
+    def __init__(self, model_name: str = "sentence-transformers/intfloat/e5-base-v2"): # или all-MiniLM-L6-v2
+       # self.model = SentenceTransformer(model_name)
+        pass
 
     def embed_text(self, text: str) -> list[float]:
         return self.model.encode(text, convert_to_numpy=True).tolist()
