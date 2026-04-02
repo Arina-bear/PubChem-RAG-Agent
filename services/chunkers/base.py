4a0f2c5626ff4c2460c0b53814bbf471df97208d
Arina <azerkalova@internet.ru>
2026-04-02 23:41:28 +0700

    Удаление ненужных папок

--- services/chunkers/base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import List
-
-class TextChunker(ABC):
-    @abstractmethod
-    def chunk(self, text: str) -> List[str]:
-        """Split the input text into meaningful chunks."""
-        pass
